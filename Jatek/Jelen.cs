5fbde6f5124c88e064c949e356c788560fde0cd0
Stiles01 <nagyv0730@gmail.com>
2020-02-20 18:32:02 +0100

    class kezdés

---
+++ Jatek/Jelen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jatek
+{
+    class Jelen
+    {
+        public string Szoba { get; set; }
+        public string Bútor { get; set; }
+        public List<string> Leltár = new List<string>();
+
+    }
+}
