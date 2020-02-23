bb865dde10b8dcf34432df2650c9b8814eef3a13
Stiles01 <nagyv0730@gmail.com>
2020-02-23 16:49:59 +0100

    02

--- Jatek/Jelen.cs
+++ Jatek/Jelen.cs
@@ -8,7 +8,7 @@ namespace Jatek
 {
     class Jelen
     {
-        
+        public 
 
     }
 }
--- Jatek/Program.cs
+++ Jatek/Program.cs
@@ -8,7 +8,7 @@ namespace Jatek
 {
     class Program
     {
-        public static void Betolt(string sor)
+        static void Betolt(string sor)
         {
             if (sor == "új")
             {
@@ -16,7 +16,10 @@ namespace Jatek
                 Targy Doboz = new Targy("nappali", "nyisd;nézd", false);
                 Targy Kulcs = new Targy("nappali", "vedd fel;tedd le;nézd", false);
                 Targy Ajto = new Targy("nappali", "nyisd;nézd", true);
-                Targy Ablak = new Targy("nappali", "nyisd;nézd;húzd", false);
+                Targy Ablak = new Targy("nappali", "nézd;törd", false);
+                Targy Furdokad = new Targy("furdo", "nézd", false);
+                Targy Feszitovas = new Targy("furdo", "nézd;vedd fel;tedd le", false);
+                
             }
             else
             {
@@ -31,15 +34,31 @@ namespace Jatek
                 }
             }
         }
+
+        static string Mentes()
+        {
+            string a;
+            return a = "";
+        }
+
+        
         static void Main(string[] args)
         {
             //Készítette: Nagy Viktória
             //Játék neve: Szabaduló-szoba
-             
 
-            Console.WriteLine("Új játékot kezdesz vagy betöltöd a már meglevő mentésed?\núj/mentés");
+            bool Keszvane = false;
+
+            Console.WriteLine("Új játékot kezdesz vagy betöltöd a már meglevő mentésed?\núj/mentés");          
             Betolt(Console.ReadLine());
-            
+            Console.WriteLine("Ha bármikor elakadnál nyomd meg a \"?\" billentyűt!");
+            while (!Keszvane)
+            {
+                string parancs = Console.ReadLine().Trim();
+                string[] tobbparancs = parancs.Split(' ');
+            }
+
+            Console.ReadKey();
             
 
         }
